09ff36d80d63c3f48228109d8ed65c271e175053
yunusemrekaplan <yunuskaplan_44@hotmail.com>
2022-11-15 19:26:10 +0300

    update

--- EmployeeProjesi/src/Main.java
+++ EmployeeProjesi/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Employee em1 = new Employee("Yunus Emre Kaplan", 14350, 100, 2020);
-        em1.ToString();
+        Employee e1 = new Employee("Yunus Emre Kaplan", 14350, 100, 2020);
+        e1.ToString();
+
     }
 }
